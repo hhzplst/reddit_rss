762f776af35129774105e869a822168168834b22
Ivy Chang <ivycwq@icloud.com>
2016-01-06 15:11:50 -0800

    creating fav functionality

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,8 +1,14 @@
 class FeedsController < ApplicationController
+  $favFeeds = Array.new
   def index
     url = "https://www.reddit.com/.rss"
     feed = Feedjira::Feed.fetch_and_parse url
     @title = feed.title
     @feeds = feed.entries
+  end
+
+  def fav
+    @favFeeds = $favFeeds.push params[:entry_title]
+    @favFeeds = @favFeeds.uniq 
   end
 end
